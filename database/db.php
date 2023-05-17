04d5d116773e1bffad7566a16ddab685b2ad4a6c
Franz2610 <francescofroechlich1@gmail.com>
2023-05-17 17:33:02 +0200

    .

--- database/db.php
+++ database/db.php
@@ -1,7 +1,7 @@
 <?php
 
-include __DIR__ . '../models/product.php';
-include __DIR__ . '../models/productAnimal.php';
+include __DIR__ . '/../models/product.php';
+include __DIR__ . '/../models/productAnimal.php';
 
 $products= [
     new Product('Guinzaglio', '12.99', 'https://tse4.mm.bing.net/th?id=OIP.LrKqKsgKw6lcmbubIc-3lwHaHa&pid=Api&P=0&h=180'),
