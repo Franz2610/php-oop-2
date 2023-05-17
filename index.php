836fa1dc694d61e5b6fd90069299b5031e4247e5
Franz2610 <francescofroechlich1@gmail.com>
2023-05-17 13:55:14 +0200

    Header & Footer

---
+++ index.php
@@ -0,0 +1,35 @@
+<?php
+
+include __DIR__ . '/view/header.php';
+
+
+foreach($products as $product)
+{?>
+
+
+<!--<div>
+    <h1>
+        <?php echo ?>
+    </h1>
+    <h2>
+        
+        <?php echo ?>
+    </h2>
+    <h3>
+        
+        <?php echo ?>
+        
+    </h3>
+    <h4>
+        
+        <?php echo ?>
+    </h4>
+</div>-->
+
+<?php
+}
+?>
+
+<?php
+include __DIR__ . '/view/footer.php';
+?>
---
+++ view/footer.php
@@ -0,0 +1,8 @@
+<footer class="text-end">
+    <h5>With &hearts; by Classe#92</h5>
+    <h6>By Francesco Froechlich</h6>
+</footer>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
+  </body>
+</html>
